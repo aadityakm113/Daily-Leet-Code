90182dad2a3085b00a68c6531e864c041d0a5205
aadityakm113 <aadityakm113@gmail.com>
2023-12-28 23:27:12 +0530

    28th-Dec

---
+++ Longest-Common-Prefix.py
@@ -0,0 +1,14 @@
+#https://leetcode.com/problems/longest-common-prefix/
+# Write a function to find the longest common prefix string amongst an array of strings.
+# If there is no common prefix, return an empty string "".
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        prefix=''
+        arr=sorted(strs)
+        first=arr[0]
+        last=arr[-1]
+        for i in range(min(len(first),len(last))):
+            if(first[i]!=last[i]):
+                return prefix
+            prefix+=first[i]
+        return prefix
