51bbeb1c32d8f30f4e3502c440499490966b0744
aadityakm113 <aadityakm113@gmail.com>
2024-10-17 18:35:22 +0530

    17th Oct

--- ValidPalindrome.py
+++ ValidPalindrome.py
@@ -6,4 +6,24 @@ class Solution:
             if i.isalnum():
                 x+=i.lower()
         return x==x[::-1]
+
+# Solution 2
+#with helper function check ascii value
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l,r=0,len(s)-1
+        while l<r:
+            while l<r and not self.alphaNum(s[l]):
+                l+=1
+            while r>l and not self.alphaNum(s[r]):
+                r-=1
+            if s[l].lower()!=s[r].lower():
+                return False
+            l+=1
+            r-=1    
+        return True
+    def alphaNum(self,x):
+        return (ord("a")<= ord(x) <= ord("z") or
+        ord("A")<= ord(x) <= ord("Z") or
+        ord("0")<= ord(x) <= ord("9"))
         
