acde862ec325a7d8712e4bd279be2e417fe76c07
aadityakm113 <aadityakm113@gmail.com>
2024-11-04 21:40:59 +0530

    4th Nov

---
+++ LengthOfLastWord.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/length-of-last-word
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        i=len(s)-1
+        count=0
+        while s[i]==" ":
+            i-=1
+        while s[i]!=" " and i>=0:
+            count+=1
+            i-=1
+        return count
