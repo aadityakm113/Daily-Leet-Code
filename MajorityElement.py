b4f5f92627e30277a36aba25777820c9be4c94ea
aadityakm113 <aadityakm113@gmail.com>
2024-11-14 10:03:54 +0530

    14th Nov

--- MajorityElement.py
+++ MajorityElement.py
@@ -9,3 +9,13 @@ class Solution:
             res=n if map[n]>maxCount else res
             maxCount=max(map[n],maxCount)
         return res
+    
+# Boyer Moore
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        res,count=0,0
+        for n in nums:
+            if count==0:
+                res=n
+            count+=(1 if n==res else -1)
+        return res
