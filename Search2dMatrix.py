a971cf3fa377d7105f130b6e77e8bcb8f11d21cf
aadityakm113 <aadityakm113@gmail.com>
2024-11-20 10:12:00 +0530

    20th Nov

--- Search2dMatrix.py
+++ Search2dMatrix.py
@@ -1,3 +1,4 @@
+# O(mlogn)
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         for i in matrix:
@@ -13,3 +14,30 @@ class Solution:
                     return True
         return False
     
+O(logm)+O(logn)=O(logmn)
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        ROWS,COLS=len(matrix),len(matrix[0])
+
+        top,bottom=0,ROWS-1
+        while top<=bottom:
+            row=(top+bottom)//2
+            if target>matrix[row][-1]:
+                top=row+1
+            elif target<matrix[row][0]:
+                bottom=row-1
+            else:
+                break
+        if not (top<=bottom):
+            return False
+        row=(top+bottom)//2
+        l,r=0,COLS-1
+        while l<=r:
+            mid=(l+r)//2
+            if target<matrix[row][mid]:
+                r=mid-1
+            elif target>matrix[row][mid]:
+                l=mid+1
+            else:
+                return True
+        return False
